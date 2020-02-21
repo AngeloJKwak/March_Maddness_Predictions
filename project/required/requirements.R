597d18e7421891232c1077973a3bdb6fe4a374c3
Angelo Kwak <angelokwak@Angelos-MacBook-Pro.local>
2020-02-21 16:01:17 -0500

    Updated for initial round one submission

--- project/required/requirements.R
+++ project/required/requirements.R
@@ -2,4 +2,3 @@ install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/") #install
 install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/") # install optparse package
 install.packages("Metrics",repos= "http://lib.stat.cmu.edu/R/CRAN/") # install metrics package
 install.packages("caret", repos = "http://topepo.github.io/caret/index.html") #install caret package
-
