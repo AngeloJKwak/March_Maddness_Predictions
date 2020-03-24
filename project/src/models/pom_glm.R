b1605749ee62f404fbe24b157f7ac4ad93f7deb1
Angelo Kwak <angelokwak@Angelos-MBP.hsd1.pa.comcast.net>
2020-03-24 11:09:44 -0400

    Feature Engineering Revisions

--- project/src/models/pom_glm.R
+++ project/src/models/pom_glm.R
@@ -1,72 +1,65 @@
-library(caret) #http://topepo.github.io/caret/index.html
 library(data.table)
+library(caret)
+library(Metrics)
+library(glmnet)
+library(plotmo)
+library(lubridate)
 
 
-set.seed(77)
+#read in data, notice the path will always look like this because the assumed working directory is the repo level folder
+train<-fread("./project/volume/data/interim/train.csv")
+test<-fread("./project/volume/data/interim/test.csv")
+example_sub<-fread("./project/volume/data/raw/example_sub.csv")
 
-train<-fread('./project/volume/data/interim/train.csv')
-test<-fread('./project/volume/data/interim/test.csv')
+##########################
+# Prep Data for Modeling #
+##########################
 
+
+# subset out only the columns to model
+
+drops<- c('team_1','team_2')
+
+train<-train[, !drops, with = FALSE]
+test<-test[, !drops, with = FALSE]
+
+
+#save the response var because dummyVars will remove
 train_y<-train$result
 
-train$team_1<-NULL
-train$team_2<-NULL
-test$team_1<-NULL
-test$team_2<-NULL
-
-
+# work with dummies
 
 dummies <- dummyVars(result ~ ., data = train)
 train<-predict(dummies, newdata = train)
 test<-predict(dummies, newdata = test)
 
-#reformat after dummyVars and add back response Var
+#fit a logistic model
+gl_model_cv<-cv.glmnet(train, train_y, alpha = 1,family="binomial")
 
-train<-data.table(train)
-train$result<-train_y
-test<-data.table(test)
+bestlam<-gl_model_cv$lambda.min
 
-
-#fit a linear model
-glm_model<-glm(result~.,family=binomial,data=train)
-
-
-#assess model
-summary(glm_model)
-
-
+gl_model<-glmnet(train, train_y, alpha = 1,family="binomial")
 
 #save model
-saveRDS(dummies,"./project/volume/models/POM_glm.dummies")
-saveRDS(glm_model,"./project/volume/models/POM_glm.model")
+saveRDS(gl_model,"./project/volume/models/gl_model.model")
 
-pred<-predict(glm_model,newdata = test,type="response")
+test<-as.matrix(test)
 
-test<-fread('./project/volume/data/interim/test.csv')
-test$result<-pred
-#our file needs to follow the example submission file format. So we need to only have the Id and saleprice column and
-#we also need the rows to be in the correct order
+#use the full model
+pred<-predict(gl_model,s=bestlam, newx = test,type="response")
 
-sub<-fread('./project/volume/data/raw/examp_sub.csv')
-sub$order<-1:nrow(sub)
-teams<-data.table(matrix(unlist(strsplit(sub$id,"_")),ncol=2,byrow=T))
-setnames(teams,c("V1","V2"),c("team_1","team_2"))
+bestlam
+predict(gl_model,s=bestlam, newx = test,type="coefficients")
+gl_model
 
-sub$team_1<-teams$team_1
-sub$team_2<-teams$team_2
+#########################
+# make a submision file #
+#########################
 
-sub$team_1<-as.character(sub$team_1)
-sub$team_2<-as.character(sub$team_2)
-test$team_1<-as.character(test$team_1)
-test$team_2<-as.character(test$team_2)
+#our file needs to follow the example submission file format.
+#we need the rows to be in the correct order
 
-sub$Result<-NULL
-
-submit<-merge(sub,test,all.x=T, by=c("team_1","team_2"))
-
-submit<-submit[order(order)]
-
-submit<-submit[,.(id,result)]
+example_sub$result<-pred
 
 #now we can write out a submission
-fwrite(submit,"./project/volume/data/processed/submit_POM.csv")
+fwrite(example_sub,"./project/volume/data/processed/submission.csv")
